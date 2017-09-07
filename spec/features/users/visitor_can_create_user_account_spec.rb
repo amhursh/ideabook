91ca7b35265355fec6af14f90fcf1cb4d90ab0ff
amhursh <amhursh@gmail.com>
2017-09-07 13:48:33 -0600

    Build new user feature test

---
+++ spec/features/users/visitor_can_create_user_account_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "visitor becomes user" do
+  context "visitor creates new user account" do
+    scenario "guest provides information to create account" do
+      visit '/'
+      click_on "Sign Up"
+      fill_in "user[username]", with: "Aaron"
+      fill_in "user[email]", with: "aaron@aaron.com"
+      fill_in "user[password]", with: "12345"
+      click_button "Create Account"
+
+      expect(current_path).to eq user_path(User.last)
+      expect(page).to have_content("Welcom, Aaron")
+    end
+  end
+end

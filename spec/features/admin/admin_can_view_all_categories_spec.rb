61e2c559b4f459f6735efbad1059f397e880ca0f
amhursh <amhursh@gmail.com>
2017-09-09 01:40:32 -0600

    Add feature test for admin authorization

---
+++ spec/features/admin/admin_can_view_all_categories_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "User visits categories index page" do
+	context "as admin" do
+		scenario "and can see all categories" do
+			admin = create(:user, role: "admin")
+			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+			visit admin_categories_path
+
+			expect(page).to have_content("Categories")
+		end
+	end
+end

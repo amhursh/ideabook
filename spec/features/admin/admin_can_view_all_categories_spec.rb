9518aebe271b5e72d3d608d84ff710ee71a810e7
amhursh <amhursh@gmail.com>
2017-09-09 02:12:21 -0600

    Add user cannot access categories to spec

--- spec/features/admin/admin_can_view_all_categories_spec.rb
+++ spec/features/admin/admin_can_view_all_categories_spec.rb
@@ -11,4 +11,15 @@ describe "User visits categories index page" do
 			expect(page).to have_content("Categories")
 		end
 	end
+
+	context "as user" do
+		scenario "and sees 404" do
+			user = create(:user)
+			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+			visit admin_categories_path
+
+			expect(page).to_not have_content("Categories")
+		end
+	end
 end

599f5826957d952692fcd27556e9fb280dbb0189
amhursh <amhursh@gmail.com>
2017-09-10 01:59:16 -0600

    Add idea validations

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,9 @@
 class Idea < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :user_id, presence: true
+  validates :category_id, presence: true
 end

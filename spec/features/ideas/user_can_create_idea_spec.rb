1afc78e54f8a33c60daf564d69841f9f84a980e4
amhursh <amhursh@gmail.com>
2017-09-09 22:47:44 -0600

    Add new idea path to user show

---
+++ spec/features/ideas/user_can_create_idea_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "User creates idea" do
+	context "user clicks on link to create new idea" do
+		scenario "user fills in information to create idea" do
+			user = create(:user)
+			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+			visit user_path(user)
+
+			click_on "Create an Idea"
+
+			fill_in "idea[title]", with: "Good Idea"
+			fill_in "idea[body]", with: "This is an Idea's description"
+			click_on "Create Idea"
+
+			expect(current_path).to eq user_idea_path(user, user.ideas.first)
+			expect(page).to have_content("Good Idea")
+			expect(page).to have_content("This is an Idea's description")
+		end
+	end
+end

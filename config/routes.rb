a7772497dbef6bb5793cca76617469fdd59aa9b4
amhursh <amhursh@gmail.com>
2017-09-09 22:49:10 -0600

    Add new user idea path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :users, only: [:new, :create, :show] do
-  	resources :ideas, only: [:index]
+  	resources :ideas, only: [:index, :new]
   end
 
   get '/login', to: "sessions#new"

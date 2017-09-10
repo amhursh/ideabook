effbf7bffa00c0d8ce9050c02ff8e0fcee411aa5
amhursh <amhursh@gmail.com>
2017-09-09 23:05:52 -0600

    Add create idea route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :users, only: [:new, :create, :show] do
-  	resources :ideas, only: [:index, :new]
+  	resources :ideas, only: [:index, :new, :create]
   end
 
   get '/login', to: "sessions#new"

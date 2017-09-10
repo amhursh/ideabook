b0cee9c87e661786faa54ea73778dd67fbc04832
amhursh <amhursh@gmail.com>
2017-09-10 16:29:39 -0600

    Add feature test for new admin category

---
+++ spec/features/categories/admin_can_create_new_category_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Admin can create new category" do
+	context "admin visits user page" do
+		scenario "and clicks link and fills in information to create new category" do
+			admin = create(:user, role: "admin")
+			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+			visit user_path(admin)
+
+			click_on "Create New Category"
+
+			expect(current_path).to eq new_admin_category_path
+
+			fill_in "category[name]", with: "Awesome Category"
+			click_on "Create Category"
+
+			expect(current_path).to eq admin_categories_path
+			expect(page).to have_content("Awesome Category")
+		end
+	end
+end

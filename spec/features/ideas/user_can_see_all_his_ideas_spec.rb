0ddd1ca12fcfcc5246ae432817ddd7975f0d184f
amhursh <amhursh@gmail.com>
2017-09-10 02:32:06 -0600

    Update to passing

--- spec/features/ideas/user_can_see_all_his_ideas_spec.rb
+++ spec/features/ideas/user_can_see_all_his_ideas_spec.rb
@@ -4,15 +4,16 @@ describe "User visits user show page" do
 	context "user clicks on link to ideas" do
 		scenario "user can see all associated ideas" do
 			user = create(:user)
-			idea = user.ideas.create(title: "hello", body: "this is a description of the idea")
+			category = create(:category)
+			idea = user.ideas.create(title: "hello", body: "this is a description of the idea", category: category)
 			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
 			visit user_path(user)
 
 			click_on "See All Ideas"
+			save_and_open_page
 
 			expect(current_path).to eq user_ideas_path(user)
-			expect(page).to have_content(idea.id)
 			expect(page).to have_content(idea.title)
 			expect(page).to have_content("All ideas for #{user.username}")
 		end

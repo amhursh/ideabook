cbba8861708d7b928fee824edd51160385753bde
amhursh <amhursh@gmail.com>
2017-09-10 12:32:29 -0600

    Add user idea destroy path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :users, only: [:new, :create, :show] do
-  	resources :ideas, only: [:index, :new, :create, :show, :edit, :update]
+  	resources :ideas
   end
 
   get '/login', to: "sessions#new"

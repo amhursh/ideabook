834bc736fa492341140645191c3d1115e9ebd1b3
amhursh <amhursh@gmail.com>
2017-09-10 23:31:15 -0600

    Add create admin images path

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
   	resources :categories, only: [:index, :new, :create, :destroy]
-    resources :images, only: [:new, :index]
+    resources :images, only: [:new, :index, :create]
   end
 
 end

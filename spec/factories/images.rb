926b2576e5b2d396adcf34835c6d2a6153fb0473
amhursh <amhursh@gmail.com>
2017-09-11 00:36:03 -0600

    Refactor with sequence and paperclip image

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :image do
-    name "MyString"
-    image ""
+    
+    sequence :name do |i| 
+    	"Bunny#{i}"
+    end
+
+    image { File.open(Rails.root.join('spec', 'fixtures', 'rabbit.jpg')) }
+  
   end
 end

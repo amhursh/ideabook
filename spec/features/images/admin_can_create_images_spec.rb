d0186a0a13a3369f6005c315d686f8a53a52e116
amhursh <amhursh@gmail.com>
2017-09-11 00:13:25 -0600

    New image creation functionality complete

--- spec/features/images/admin_can_create_images_spec.rb
+++ spec/features/images/admin_can_create_images_spec.rb
@@ -16,6 +16,8 @@ describe "Admin can create new image" do
 			attach_file("Attach an Image", fixture_image_path)
 			click_on "Create Image"
 
+			save_and_open_page
+
 			expect(current_path).to eq admin_images_path
 			expect(page).to have_content("Bunny Rabbit")
 			expect(page).to have_xpath("//img[contains(@src, 'rabbit.jpg')]")

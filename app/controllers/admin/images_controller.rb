35b3292cd9341eba7b5ccb5436192ea3caec3275
amhursh <amhursh@gmail.com>
2017-09-10 23:57:11 -0600

    Add create to images controller and validations to model

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -4,4 +4,19 @@ class Admin::ImagesController < Admin::BaseController
 		@image = Image.new
 	end
 
+	def create
+		@image = Image.new(image_params)
+		if @image.save
+			redirect_to admin_images_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def image_params
+		params.require(:image).permit(:name, :image)
+	end
+
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,6 @@ class Image < ApplicationRecord
 	has_many :idea_images
 	has_many :ideas, through: :idea_images
 
-	has_attached_file :image, styles: { standard: "300x300" }
+	has_attached_file :image, styles: { standard: "300x300>" }
+	validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg"]
 end

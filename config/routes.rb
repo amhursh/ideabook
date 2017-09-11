b3df0effe3ca489339a20690e6b1db2d366b7f27
amhursh <amhursh@gmail.com>
2017-09-10 23:29:39 -0600

    Add admin images route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
   	resources :categories, only: [:index, :new, :create, :destroy]
-    resources :images, only: [:new]
+    resources :images, only: [:new, :index]
   end
 
 end

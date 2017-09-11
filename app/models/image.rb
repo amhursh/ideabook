c254b8fa8d5ecd7cb75eb38e1b976c7b7e817867
amhursh <amhursh@gmail.com>
2017-09-11 00:58:01 -0600

    Add validations for name and attachment presence

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,7 @@ class Image < ApplicationRecord
 
 	has_attached_file :image, styles: { standard: "300x300>" }
 	validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg"]
+	validates :name, presence: true
+	validates :image, attachment_presence: true
+
 end
